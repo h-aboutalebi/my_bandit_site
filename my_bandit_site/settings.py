70c956bf6db17b9ff0a3b150e5218c24a155e18f
h-aboutalebi <hossain.aboutalebi@gmail.com>
2018-11-12 13:24:48 -0500

    yes

--- my_bandit_site/settings.py
+++ my_bandit_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 # Configure Django App for Heroku.
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,6 +125,6 @@ STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'polls/templates'),)
 
-import django_heroku
+
 django_heroku.settings(locals())
 
